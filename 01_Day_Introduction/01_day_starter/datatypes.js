ade3b35b91c6485b08e0b88b9b9b40ac4d0cb868
Dian Reyna <reynaarmas@gmail.com>
2022-09-19 16:32:13 -0500

    Day 01 of  30 finished

---
+++ 01_Day_Introduction/01_day_starter/datatypes.js
@@ -0,0 +1,23 @@
+let firstName = "dian";
+let lastName = "reyna";
+let isMarried = false;
+let country = "Mexico";
+let age = 29;
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof isMarried);
+console.log(typeof country);
+console.log(typeof age);
+
+let nombre = "dian",
+  apellido = "reyna",
+  estadoCivil = false,
+  pais = "Mexico",
+  edad = 29;
+
+let miEdad = 29;
+let tuEdad = 20;
+
+console.log("Tengo", miEdad, "años.");
+console.log("Tu tienes", tuEdad, "años.");
--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,4 +1,8 @@
 // the variable values can be accessed from other variable.js file
-console.log(firstName, lastName, country, city, age, isMarried) 
-console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
-console.log(name, job, live)
+console.log(firstName, lastName, country, city, age, isMarried);
+console.log(gravity, boilingPoint, PI); // 9.81, 100, 3.14
+console.log(name, job, live);
+
+//los comentarios pueden hacer que el código sea legible
+//Bienvenido a 30DaysOfJavaScript
+/* Los comentarios pueden hacer el código legible, fácil de usar e informativo */
